d842917a1a2abe1b388a9ed527254056d149c4fa
jamltg <jamletigio@gmail.com>
2024-05-09 13:26:02 +0800

    Blog website

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.CONNECTION_STRING)
   .then(() => {
     console.log('MongoDb is connected');
   })
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "blog-website-c8a5a.firebaseapp.com",
+  projectId: "blog-website-c8a5a",
+  storageBucket: "blog-website-c8a5a.appspot.com",
+  messagingSenderId: "791519505679",
+  appId: "1:791519505679:web:b4617680b3cc07f4dfe27a"
 };
 
 // Initialize Firebase
